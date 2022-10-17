9a9c4707715b09c36325526aede248bd74516710
thuylvph18228 <anhthuy03072002@gmail.com>
2022-10-17 20:17:21 +0700

    validate crud nhan vien

--- src/main/java/poly/edu/Controller/NhanVienController.java
+++ src/main/java/poly/edu/Controller/NhanVienController.java
@@ -3,6 +3,7 @@ package poly.edu.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import poly.edu.DAO.NhanVienDAO;
 import poly.edu.Entity.ChucVu;
 import poly.edu.Entity.NhanVien;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -58,8 +60,12 @@ public class NhanVienController {
     }
 
     @PostMapping("/savenv")
-    public String savenv(@ModelAttribute("nhanvien") NhanVien nhanvien){
-        nvdao.save(nhanvien);
-        return "redirect:/nhanvien/index";
+    public String savenv(@Valid @ModelAttribute("nhanvien") NhanVien nhanvien, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "nhanvien/save";
+        }else {
+            nvdao.save(nhanvien);
+            return "redirect:/nhanvien/index";
+        }
     }
 }
