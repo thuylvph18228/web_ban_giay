854d02d7cf2166c3e5c8eabe82dcf132bb873a00
phonghoa1312 <phonghoa1312@gmail.com>
2022-10-20 01:36:56 +0700

    commit

--- src/main/java/poly/edu/DAO/NsxDAO.java
+++ src/main/java/poly/edu/DAO/NsxDAO.java
@@ -3,6 +3,6 @@ package poly.edu.DAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import poly.edu.Entity.NSX;
 
-public interface NsxDAO extends JpaRepository<NSX, Integer> {
+public interface NSXDAO extends JpaRepository <NSX,Integer> {
 
 }
