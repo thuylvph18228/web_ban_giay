63dbc3319f72c861278271ede83ffecee015e7eb
thienndph18157 <thienndph18157@fpt.edu.vn>
2022-10-20 18:16:53 +0700

    chức năng đăng nhập

--- src/main/java/poly/edu/DAO/NhanVienDAO.java
+++ src/main/java/poly/edu/DAO/NhanVienDAO.java
@@ -1,8 +1,9 @@
 package poly.edu.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import poly.edu.Entity.KhachHang;
 import poly.edu.Entity.NhanVien;
 
 public interface NhanVienDAO extends JpaRepository<NhanVien, Integer> {
-
+    public NhanVien findByEmailEquals(String email) ;
 }
