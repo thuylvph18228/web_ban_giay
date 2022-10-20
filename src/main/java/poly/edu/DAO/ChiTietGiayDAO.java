90bb361bb1e8037dfc062f41243710dfb56e130b
thuylvph18228 <anhthuy03072002@gmail.com>
2022-10-20 17:09:10 +0700

    no message

--- src/main/java/poly/edu/DAO/ChiTietGiayDAO.java
+++
@@ -1,8 +0,0 @@
-package poly.edu.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import poly.edu.Entity.ChiTietGiay;
-
-public interface ChiTietGiayDAO extends JpaRepository<ChiTietGiay, Integer> {
-
-}
--- src/main/java/poly/edu/DAO/ChiTietHoaDonDAO.java
+++
@@ -1,7 +0,0 @@
-package poly.edu.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import poly.edu.Entity.ChiTietHoaDon;
-
-public interface ChiTietHoaDonDAO extends JpaRepository<ChiTietHoaDon,Integer> {
-}
