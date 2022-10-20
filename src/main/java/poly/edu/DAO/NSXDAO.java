81eda28b0f77b1d22f19594d99a40745c37fe03d
thuylvph18228 <anhthuy03072002@gmail.com>
2022-10-20 11:10:47 +0700

    no message

--- src/main/java/poly/edu/DAO/NSXDAO.java
+++ src/main/java/poly/edu/DAO/NSXDAO.java
@@ -3,6 +3,5 @@ package poly.edu.DAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import poly.edu.Entity.NSX;
 
-public interface NSXDAO extends JpaRepository <NSX,Integer> {
 
 }
