bbe5bf0b82d4a6a01fb827da98f2cee27958df59
thuylvph18228 <80585222+thuylvph18228@users.noreply.github.com>
2022-10-20 15:55:58 +0700

    Revert "crud nhan vien, giay, mau sac, loai giay, chi tiet giay"

--- src/main/java/poly/edu/DAO/NsxDAO.java
+++
@@ -1,7 +0,0 @@
-package poly.edu.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import poly.edu.Entity.Nsx;
-
-public interface NsxDAO extends JpaRepository<Nsx, Integer> {
-}
--- src/main/java/poly/edu/DAO/SizeDAO.java
+++
@@ -1,8 +0,0 @@
-package poly.edu.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import poly.edu.Entity.Size;
-
-
-public interface SizeDAO extends JpaRepository<Size, Integer> {
-}
