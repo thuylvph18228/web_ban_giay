df1485e9a3dedad2bf7608073be81b21030c8a80
thuylvph18228 <anhthuy03072002@gmail.com>
2022-10-20 14:45:54 +0700

    fist commit

--- src/main/java/poly/edu/DAO/GiayDAO.java
+++
@@ -1,8 +0,0 @@
-package poly.edu.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import poly.edu.Entity.Giay;
-
-public interface GiayDAO extends JpaRepository<Giay, Integer> {
-
-}
--- src/main/java/poly/edu/DAO/KhachHangDAO.java
+++
@@ -1,7 +0,0 @@
-package poly.edu.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import poly.edu.Entity.KhachHang;
-
-public interface KhachHangDAO  extends JpaRepository<KhachHang, Integer> {
-}
