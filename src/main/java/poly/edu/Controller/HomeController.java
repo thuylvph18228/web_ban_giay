0cb1dbfbbb20fb4d996e81007522c97252afa959
thuylvph18228 <anhthuy03072002@gmail.com>
2022-10-23 10:01:46 +0700

    m

---
+++ src/main/java/poly/edu/Controller/HomeController.java
@@ -0,0 +1,21 @@
+package poly.edu.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import poly.edu.DAO.GiayDAO;
+import poly.edu.Entity.Giay;
+
+import java.util.List;
+
+public class HomeController {
+    @Autowired
+    GiayDAO giaydao;
+
+    @GetMapping("/")
+    public String index(Model model){
+        List<Giay> listgl = giaydao.findAll();
+        model.addAttribute("listgl", listgl);
+        return "/layout";
+    }
+}
