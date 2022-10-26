10f921c1a28a852ede02a3df48a696f7b797964d
thienndph18157 <thienndph18157@fpt.edu.vn>
2022-10-26 11:18:04 +0700

    update thêm sp vào giỏ hàng

--- src/main/java/poly/edu/DAO/ChiTietGiayDAO.java
+++ src/main/java/poly/edu/DAO/ChiTietGiayDAO.java
@@ -4,14 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import poly.edu.Entity.ChiTietGiay;
-import poly.edu.Entity.SizeTheoGiay;
 
 import java.util.List;
     @Repository
 public interface ChiTietGiayDAO extends JpaRepository<ChiTietGiay, Integer> {
     @Query("SELECT e FROM ChiTietGiay e WHERE e.mag = ?1")
     List<ChiTietGiay> findByMag(int mag);
-      @Query("select e.mas from ChiTietGiay e join Giay g on e.mag =  g.mag where  g.mag =?1")
-      List<SizeTheoGiay> findByMas(int mag);
 
 }
