0243c999436ef310367d0f0ae452ab6efe1c6dc5
thienndph18157 <thienndph18157@fpt.edu.vn>
2022-11-08 12:47:12 +0700

    no message

---
+++ src/main/java/poly/edu/Entity/TraHang.java
@@ -0,0 +1,36 @@
+package poly.edu.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+
+@Entity
+@Data
+@Table(name = "chitiethoadon")
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChiTietHoaDon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="macthd")
+    private int macthd;
+
+    @Column(name="mactg")
+    private int mactg;
+
+    @Column(name="mahd")
+    private int mahd;
+
+//    @NotBlank(message = "Không được để trống tổng tiền")
+//    @Min(value = 1, message = "Tổng tiền phải lớn hơn 0")
+
+//    @NotBlank(message = "Không được để trống số lượng")
+//    @Min(value = 1, message = "Số lượng phải lớn hơn 0")
+    @Column(name="soluong")
+    private int soluong;
+
+}
