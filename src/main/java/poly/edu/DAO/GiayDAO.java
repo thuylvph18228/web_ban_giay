edfdae9e7a4384a571fb130dda15e6f71f6f636c
thienndph18157 <thienndph18157@fpt.edu.vn>
2022-11-12 16:37:10 +0700

    hiển thị top giày mới và top giày bán chạy

--- src/main/java/poly/edu/DAO/GiayDAO.java
+++ src/main/java/poly/edu/DAO/GiayDAO.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 import poly.edu.Entity.Giay;
 import poly.edu.Entity.GioHang;
 import poly.edu.Entity.HoaDon;
@@ -12,6 +13,7 @@ import poly.edu.Entity.KhachHang;
 import java.util.Collections;
 import java.util.List;
 
+@Repository
 public interface GiayDAO extends JpaRepository<Giay, Integer> {
     public Giay findByTenEquals(String ten) ;
 
@@ -20,8 +22,17 @@ public interface GiayDAO extends JpaRepository<Giay, Integer> {
 
     @Query("SELECT e FROM Giay e WHERE e.ten = ?1")
     List<Giay> findByName(String teng);
+    @Query(value ="SELECT * from Giay g order by  g.mag desc limit 4 ", nativeQuery = true )
+    List<Giay> findByTop5New();
 
-    @Query("SELECT e FROM Giay e WHERE e.ten LIKE %?1%")
+    @Query(value = "SELECT * FROM Giay g \n" +
+            "join ChiTietGiay ctg on ctg.mag  = g.mag\n" +
+            "join ChiTietHoaDon c on c.mactg = ctg.mactg \n" +
+            "group by g.mag  \n" +
+            "order by sum(ctg.soluong) desc LIMIT 4"  ,nativeQuery = true)
+    List<Giay> findBySelling( );
+
+    @Query( value = "SELECT e FROM Giay e WHERE e.ten LIKE %?1%" ,nativeQuery = true)
     Page<Giay> findByNameLike(String name, Pageable pageable);
 
     @Query("SELECT g FROM Giay g join ChiTietGiay c on g.mag = c.mag where c.mansx=?1 group by g.mag")
